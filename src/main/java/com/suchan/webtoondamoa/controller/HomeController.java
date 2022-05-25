86d7aa99a685649a200ef30d1de9fb3226a884bf
suchanmyung <aud248@naver.com>
2022-05-25 23:10:05 +0900

    feat : front Vue.js 연동

--- src/main/java/com/suchan/webtoondamoa/controller/HomeController.java
+++ src/main/java/com/suchan/webtoondamoa/controller/HomeController.java
@@ -4,11 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
 @Controller
 public class HomeController {
+
+    @ResponseBody
+    @RequestMapping("/api/hello")
+    public String helloWorld(){
+        return "hello!";
+    }
 
     @RequestMapping("/")
     public String index() {
